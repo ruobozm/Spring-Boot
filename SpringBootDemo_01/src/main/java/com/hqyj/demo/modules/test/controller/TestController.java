0bea69397795011d009a8566088c1b2ff415dc8b
ruobo <im.zm@outlook.com>
2019-11-26 20:00:27 +0800

    "2019.11.26 spring boot"

--- SpringBootDemo_01/src/main/java/com/hqyj/demo/modules/test/controller/TestController.java
+++ SpringBootDemo_01/src/main/java/com/hqyj/demo/modules/test/controller/TestController.java
@@ -1,11 +1,65 @@
 package com.hqyj.demo.modules.test.controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import com.hqyj.demo.modules.test.vo.ApplicationConfigTestBean;
+
 @Controller
 public class TestController {
+	
+	//通过resources根目录的Application.properties文件，直接使用@Value获得属性值
+	@Value("${server.port}")
+	private  int port;
+	@Value("${com.hqyj.name}")
+	private String name;
+	@Value("${com.hqyj.age}")
+	private int age;
+	@Value("${com.hqyj.description}")
+	private String description;
+	@Value("${com.hqyj.random}")
+	private String random;
+	
+	// 针对其他配置文件，注入其对应的配置类
+	@Autowired
+	private ApplicationConfigTestBean configTestBean;
+	
+	private final static Logger LOGGER = LoggerFactory.getLogger(TestController.class);
+
+	
+	
+	/**
+	 * <p>
+	 * 获取配置文件
+	 * </p>
+	 * @author zm
+	 * @Date 2019年11月26日
+	 * @return
+	 */
+	@RequestMapping("/test/getConfig")
+	@ResponseBody
+	public String getConfig() {
+		StringBuffer sb = new StringBuffer();
+		sb.append(port)
+		.append("----").append(name).append("<br>")
+		.append("----").append(age).append("<br>")
+		.append("----").append(description).append("<br>")
+		.append("----").append(random).append("<br>");
+		
+		sb.append(configTestBean.getName())
+		.append("‐‐‐‐").append(configTestBean.getAge()).append("<br>")
+		.append("‐‐‐‐").append("<br>")
+		.append(configTestBean.getDescription()).append("<br>")
+		.append("‐‐‐‐").append("<br>")
+		.append(configTestBean.getRandom()).append("</br>");
+
+		return sb.toString();
+	}
 	
 	@RequestMapping("/test/testInfo")
 	@ResponseBody
@@ -14,4 +68,25 @@ public class TestController {
 	}
 	
 	
+	/**
+	 * <p>
+	 * 日志文件logback
+	 * </p>
+	 * @author zm
+	 * @Date 2019年11月26日
+	 * @return
+	 */
+	@RequestMapping("/test/log")
+	@ResponseBody
+	public String loggerTest() {
+		LOGGER.trace("This is trace log");
+		LOGGER.debug("This is debug log");
+		LOGGER.info("This is info log");
+		LOGGER.warn("This is warn log");
+		LOGGER.error("This is error log");
+		
+		return "This is logger test.";
+	}
+	
+
 }
--- SpringBootDemo_01/src/main/java/com/hqyj/demo/modules/test/vo/ApplicationConfigTestBean.java
+++ SpringBootDemo_01/src/main/java/com/hqyj/demo/modules/test/vo/ApplicationConfigTestBean.java
@@ -1,5 +1,45 @@
 package com.hqyj.demo.modules.test.vo;
 
-public class ApplicationConfigTestBean {
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.stereotype.Component;
 
+/**
+ * @Component ‐‐‐‐ 注册为bean
+ * @PropertySource ‐‐‐‐ 指定其他配置文件位置
+ * @ConfigurationProperties ‐‐‐‐ 指定属性公有前缀
+ */
+@Component
+@PropertySource("classpath:config/applicationTest.properties")
+@ConfigurationProperties(prefix = "com.hqyj")
+public class ApplicationConfigTestBean {
+	private String name;
+	private int age;
+	private String description;
+	private String random;
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public int getAge() {
+		return age;
+	}
+	public void setAge(int age) {
+		this.age = age;
+	}
+	public String getDescription() {
+		return description;
+	}
+	public void setDescription(String description) {
+		this.description = description;
+	}
+	public String getRandom() {
+		return random;
+	}
+	public void setRandom(String random) {
+		this.random = random;
+	}
+	
 }
